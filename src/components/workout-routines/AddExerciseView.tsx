87839102abce48f514f27461d31e89eeb2598c3f
Kirill M <k-l.m@hotmail.com>
2023-01-31 22:25:10 +0100

    added basic fuzzysort search functionality to the add exercise view

--- src/components/workout-routines/AddExerciseView.tsx
+++ src/components/workout-routines/AddExerciseView.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useEffect, useState } from 'react';
+import fuzzysort from 'fuzzysort';
 import useApi from '../../hooks/useApi';
 
 // eslint-disable-next-line react/prop-types, @typescript-eslint/no-explicit-any
@@ -6,6 +9,25 @@ function AddExerciseView({ setAddExerciseView, newWorkout, setNewWorkout }: any)
   const API_URL = 'https://exercisedb.p.rapidapi.com/exercises';
   const { data, loading, error } = useApi(API_URL);
   const [clicked, setClicked] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredExercises, setFilteredExercises] = useState(data);
+
+  useEffect(() => {
+    let APICallDelay: ReturnType<typeof setTimeout>;
+    if (data) {
+      APICallDelay = setTimeout(() => {
+        const results = fuzzysort
+          .go(searchTerm, data, { key: 'name' })
+          .map((result) => result.target);
+        setFilteredExercises(
+          data?.filter((exercise) => results.includes(exercise.name))
+        );
+      }, 1000);
+    }
+    return () => {
+      clearTimeout(APICallDelay);
+    };
+  }, [searchTerm, data]);
 
   function handleAddExercise(e: React.MouseEvent<HTMLDivElement>, name: string) {
     e.preventDefault();
@@ -22,15 +44,21 @@ function AddExerciseView({ setAddExerciseView, newWorkout, setNewWorkout }: any)
   return (
     <>
       <button type="button" onClick={() => setAddExerciseView(false)}>Done</button>
+      <br />
+
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Search for an exercise"
+      />
 
       {loading && <p>Loading. . .</p>}
 
       {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
       {error && <p>Error: {error}</p>}
 
-      {data && data?.map((exercise, index) => (
-        // eslint-disable-next-line max-len
-        // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+      {data && filteredExercises?.map((exercise, index) => (
         <div
           // eslint-disable-next-line react/no-array-index-key
           key={index}
